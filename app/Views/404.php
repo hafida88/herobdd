780455f126781101c5f67fb3dcdbe64d11d6653a
hafida88 <hafida.hazard@gmail.com>
2025-09-15 15:30:51 +0200

    creation de routeur

---
+++ app/Views/404.php
@@ -0,0 +1,16 @@
+<?php 
+require_once(__DIR__ . '/partials/head.php');
+?>
+
+<div class="card text-bg-dark">
+<div class="card text-bg-dark">
+  <img src="public/img/erreur.webp" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <h5 class="card-title d-flex justify-content-center fs-2 text-danger">Avertissement de redirection</h5>
+    <p class="card-text d-flex justify-content-center fs-4"> La page que vous consultiez essaie de vous rediriger vers https://nicepage.com/fr/k/404-modeles-html.</p>
+    <p class="card-text d-flex justify-content-center fs-4"><small> Si vous ne souhaitez pas consulter cette page, vous pouvez revenir à la page précédente.</small></p>
+  </div>
+</div>
+<?php 
+require_once(__DIR__ . '/partials/footer.php');
+?>
---
+++ app/Views/partials/footer.php
@@ -0,0 +1,3 @@
+<footer><p>@Hafida - LePoleS</p></footer>
+</body>
+</html>
