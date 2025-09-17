323a3166fa673f7ecbb418eb7d627a5fc72c90d4
hafida88 <hafida.hazard@gmail.com>
2025-09-17 17:01:08 +0200

    test

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -8,25 +8,34 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/public/css/style.css">
 </head>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Heros Projet</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+        <?php
+          if(isset($_SESSION['user'])){
+        ?>
+          <li class="nav-item">
+            <a class="nav-link" href="/logout">Se déconnecter</a>
+          </li>
+        <?php
+          } else {
+        ?>
+          <li class="nav-item">
+            <a class="nav-link" href="/register">S'inscrire</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login">Se connecter</a>
+          </li>
+        <?php
+        }
+        ?>
       </ul>
     </div>
   </div>
 </nav>
-<body>
---
+++ config/function.php
@@ -0,0 +1,28 @@
+<?php 
+    function redirectToRoute($route, $code){
+        http_response_code($code);
+        header("Location: {$route}");
+        exit;
+    }
+
+    function successMessage($myMessage){
+
+        ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <?= $myMessage ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php
+
+    }
+
+    function errorMessage($myMessage){
+
+        ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <?= $myMessage ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php
+
+    }
